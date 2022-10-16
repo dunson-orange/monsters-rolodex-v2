5c3fe2798e018a8554e479cebf2c96b3182edd64
dunson-orange <mpdunson@gmail.com>
2022-10-15 21:39:38 -0400

    Component Props

--- src/App.js
+++ src/App.js
@@ -11,23 +11,16 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    console.log("constructor");
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
-    console.log("componentDidMount");
   }
 
   onSearchChange = (event) => {
@@ -41,8 +34,6 @@ class App extends Component {
   };
 
   render() {
-    console.log("render");
-
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster) => {
@@ -57,12 +48,7 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {/*filteredMonsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        ))*/}
-        <CardList />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
